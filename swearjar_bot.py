696ac9c4ab22410a77ef7509bee60962d9ea0124
Octave Larose <octave.larose@hotmail.fr>
2018-07-15 13:21:37 +0200

    check_jar now recognizes a user, whether mentioned with @ or not

--- swearjar_bot.py
+++ swearjar_bot.py
@@ -6,7 +6,9 @@ import json
 import os
 
 bot = commands.Bot(command_prefix='%')
-bot_name = "SwearJar_Bot#2252"
+bot_name = "SwearJar_Bot"
+bot_game = "being a good bot"
+
 os.chdir(r'C:\Users\Octave\Desktop\Python Discord bot')
 
 @bot.event
@@ -14,13 +16,11 @@ async def on_ready():
     print ("Ready to go!")
     print ("My name is " + bot.user.name)
     print ("My ID is " + bot.user.id)
+    await bot.change_presence(game=discord.Game(name=bot_game))
 
 @bot.event
-async def on_message(message):
-    if message.author.bot != True:
-        if "cookie" in message.content:
-               await bot.send_message(message.channel, ":cookie:")
-    await bot.process_commands(message)
+async def on_command_error(error, ctx):
+    bot.send_message(ctx.message.channel, "The bot has fucked up, somehow. Blame <@ID>")
 
 @bot.command(pass_context=True)
 async def total_jar(ctx):
@@ -30,7 +30,7 @@ async def total_jar(ctx):
         await bot.say("The jar is empty.")
     else:
         total = users['total']['dollars']
-        string = "The total amount of money in the jar is : {}** ".format(total)
+        string = "The total amount of money in the jar is : **{} ".format(total)
         if total != 1:
             string += "dollars**"
         else:
@@ -38,15 +38,22 @@ async def total_jar(ctx):
         await bot.say(string.format(total))
 
 @bot.command(pass_context=True)
-async def check_jar(ctx, user_sent):
-    pass
+async def check_jar(ctx, user_sent:str = None):
+    if user_sent is None:
+        await bot.send_message(ctx.message.channel, "User could not be found.")
+    else:
+        if user_sent[0:2] == '<@':
+            user_sent = user_sent[2:-1]
+            user_found = ctx.message.server.get_member(user_sent)
+        else:
+            user_found = ctx.message.server.get_member_named(name=user_sent)
+        if user_found is None:
+            await bot.send_message(ctx.message.channel, "User could not be found.")
+        else:
+            await bot.send_message(ctx.message.channel, "User FOUND : {}".format(user_found.name))
 
-@check_jar.error
-async def check_jar_on_error(error, ctx):
-	await bot.send_message(ctx.message.channel, "User could not be found.")
-
-@bot.command(pass_context=True)
-async def info(ctx):
+@bot.command()
+async def info():
     with open(r'C:\Users\Octave\Desktop\Python Discord bot\info', 'r') as info_file:
         info_str = info_file.read()
     await bot.say(info_str)
@@ -80,5 +87,6 @@ async def add_dollar_count(users, user):
             users['total']['dollars'] = 0
         users['total']['dollars'] += 1
 
-
-bot.run("TOKEN")
+with open(r'C:\Users\Octave\Desktop\Python Discord bot\swearjar_token', 'r') as info_file:
+        token = info_file.read()
+bot.run(token)
