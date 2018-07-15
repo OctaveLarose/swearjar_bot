c2375957600f782aeae8543204852295e023c62a
Octave Larose <octave.larose@hotmail.fr>
2018-07-15 19:15:55 +0200

    import logging, bot now reacts when jar is checked with his username, put most of the content of swear() in add_dollar_count()

--- swearjar_bot.py
+++ swearjar_bot.py
@@ -4,6 +4,9 @@ import discord
 from discord.ext import commands
 import json
 import os
+import logging
+
+logging.basicConfig(level=logging.INFO)
 
 bot = commands.Bot(command_prefix='%')
 bot_name = "SwearJar_Bot"
@@ -20,14 +23,19 @@ async def on_ready():
 async def on_command_error(error, ctx):
     bot.send_message(ctx.message.channel, "The bot has fucked up, somehow. Blame <@ID>")
 
+@bot.event
+async def on_message(message):
+    ''
+    await bot.process_commands(message)
+
 @bot.command(pass_context=True)
 async def total_jar(ctx):
     with open('swearjar.json', 'r') as f:
-        users = json.load(f)
-    if 'total' not in users:
+        users_list = json.load(f)
+    if 'total' not in users_list:
         await bot.say("The jar is empty.")
     else:
-        total = users['total']['dollars']
+        total = users_list['total']['dollars']
         string = "The total amount of money in the jar is : **{} ".format(total)
         if total != 1:
             string += "dollars**"
@@ -38,7 +46,7 @@ async def total_jar(ctx):
 @bot.command(pass_context=True)
 async def check_jar(ctx, user_sent:str = None):
     if user_sent is None:
-        await bot.send_message(ctx.message.channel, "User could not be found.")
+        await bot.send_message(ctx.message.channel, "Please enter a username.")
     else:
         if user_sent[0:2] == '<@':
             user_sent = user_sent[2:-1]
@@ -49,13 +57,15 @@ async def check_jar(ctx, user_sent:str = None):
             user_found = ctx.message.server.get_member_named(name=user_sent)
         if user_found is None:
             await bot.send_message(ctx.message.channel, "User could not be found.")
+        elif user_found == bot.user:
+            await bot.send_message(ctx.message.channel, "Nice try, friend. But I'm the one who makes the rules here.")
         else:
             with open('swearjar.json', 'r') as f:
-                users = json.load(f)
-            if user_found.id not in users:
-                await bot.say("{} has not put a single dollar in the jar, and is either a very good boy or a liar.".format(user_found.name))
+                users_list = json.load(f)
+            if user_found.id not in users_list:
+                await bot.say("**{}** has not put a single dollar in the jar, and is either a very good boy or a liar.".format(user_found.name))
             else:
-                total = users[user_found.id]['dollars']
+                total = users_list[user_found.id]['dollars']
                 string = "**{}** has put **{} ".format(user_found.name, total)
                 if total != 1:
                     string += "dollars**"
@@ -72,32 +82,29 @@ async def info():
 
 @bot.command(pass_context=True)
 async def swear(ctx):
-    author = ctx.message.author
-
-    with open('swearjar.json', 'r') as f:
-        users = json.load(f)
-
-    if author.id not in users:
-        users[author.id] = {}
-        users[author.id]['dollars'] = 0
-
-    await add_dollar_count(users, ctx.message.author)
-
-    with open('swearjar.json', 'w') as f:
-        json.dump(users, f)
-    
+    await add_dollar_count(ctx.message.author, ctx)  
     emoji = '\N{MONEY BAG}'
     await bot.add_reaction(ctx.message, emoji)
 
-async def add_dollar_count(users, user):
-    if not user.id in users:
-        print("Error : user not listed")
-    else:
-        users[user.id]['dollars'] += 1
-        if 'total' not in users:
-            users['total'] = {}
-            users['total']['dollars'] = 0
-        users['total']['dollars'] += 1
+async def add_dollar_count(user, ctx):
+    try:
+        with open('swearjadr.json', 'r') as f:
+            users_list = json.load(f)
+            
+        if user.id not in users_list:
+            users_list[user.id] = {}
+            users_list[user.id]['dollars'] = 0
+        else:
+            users_list[user.id]['dollars'] += 1
+            if 'total' not in users_list:
+                users_list['total'] = {}
+                users_list['total']['dollars'] = 0
+            users_list['total']['dollars'] += 1
+            
+        with open('swearjar.json', 'w') as f:
+            json.dump(users_list, f)
+    except:
+        bot.send_message(ctx.message.channel, "Error : couldn't put a dollar in the jar for {}. Ask soap to fix it.".format(user.name))
 
 with open(r'C:\Users\Octave\Desktop\Python Discord bot\swearjar_token', 'r') as info_file:
         token = info_file.read()
